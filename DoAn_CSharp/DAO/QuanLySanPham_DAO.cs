84952eafc151151ecc79cc28939d7f5b1e479c14
Châu Quốc ALin <89209619+alinsbtc@users.noreply.github.com>
2023-11-21 15:10:11 +0700

    DAOtest

--- DoAn_CSharp/DAO/QuanLySanPham_DAO.cs
+++ DoAn_CSharp/DAO/QuanLySanPham_DAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,18 @@ namespace DoAn_CSharp.DAO
 {
     internal class QuanLySanPham_DAO
     {
+        private Database.Database db;
+        public QuanLySanPham_DAO()
+        {
+            db = new Database.Database();
+
+        }
+        public DataTable LayDanhSachDanhMuc()
+        {
+            string strSQL = "Select * from danhmuc";
+            DataTable dt = db.Execute(strSQL);
+            return dt;
+        }
+
     }
 }
